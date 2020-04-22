628f9c101c012c488ae748a2286bf197aab6f023
Andrei Mireichyk <>
2020-04-22 14:58:56 +0300

    video

--- resources/application/app/pages/index/index.js
+++ resources/application/app/pages/index/index.js
@@ -6,6 +6,14 @@ import "bootstrap/js/dist/modal.js"
 (new Accordion(document.querySelector('.sc-5__accordion')));
 
 $(function () {
+    $( window ).resize(function() {
+        $('.device__animation').height($('.device__phone img').width())
+        $('.device__animation').width($('.device__phone img').width())
+    });
+    $('.device__animation').height($('.device__phone img').width())
+    $('.device__animation').width($('.device__phone img').width())
+
+
     $('#video').on('hidden.bs.modal', function (e) {
         $("#video iframe")[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
     })
