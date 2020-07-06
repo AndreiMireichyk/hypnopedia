caf70dca2b095fc43de10c2d34156aece13e1f39
Andrei Mireichyk <>
2020-07-06 14:06:01 +0300

    fix site map lastmod format date

--- resources/views/app/sitemap.blade.php
+++ resources/views/app/sitemap.blade.php
@@ -5,21 +5,21 @@
             <loc>{{route('home')}}</loc>
             <priority>0.6</priority>
             <changefreq>monthly</changefreq>
-            <lastmod>{{\Carbon\Carbon::now()->format('Y-m-dTH:i:sP')}}</lastmod>
+            <lastmod>{{\Carbon\Carbon::now()->format('c')}}</lastmod>
         </url>
 
         <url>
             <loc>{{route('contacts')}}</loc>
             <priority>0.6</priority>
             <changefreq>monthly</changefreq>
-            <lastmod>{{\Carbon\Carbon::now()->format('Y-m-dTH:i:sP')}}</lastmod>
+            <lastmod>{{\Carbon\Carbon::now()->format('c')}}</lastmod>
         </url>
 
         <url>
             <loc>{{route('blog')}}</loc>
             <priority>0.6</priority>
             <changefreq>monthly</changefreq>
-            <lastmod>{{\Carbon\Carbon::now()->format('Y-m-dTH:i:sP')}}</lastmod>
+            <lastmod>{{\Carbon\Carbon::now()->format('c')}}</lastmod>
         </url>
 
         @foreach($articles_categories as $category)
@@ -27,7 +27,7 @@
                 <loc>{{route('blog.category', $category->slug)}}</loc>
                 <priority>0.6</priority>
                 <changefreq>monthly</changefreq>
-                <lastmod>{{\Carbon\Carbon::parse($category->updated_at)->format('Y-m-dTH:i:sP')}}</lastmod>
+                <lastmod>{{\Carbon\Carbon::parse($category->updated_at)->format('c')}}</lastmod>
             </url>
         @endforeach
         @foreach($articles as $article)
@@ -35,7 +35,7 @@
                 <loc>{{route('blog.article', $article->slug)}}</loc>
                 <priority>0.7</priority>
                 <changefreq>monthly</changefreq>
-                <lastmod>{{\Carbon\Carbon::parse($article->updated_at)->format('Y-m-dTH:i:sP')}}</lastmod>
+                <lastmod>{{\Carbon\Carbon::parse($article->updated_at)->format('c')}}</lastmod>
             </url>
         @endforeach
     </urlset>
