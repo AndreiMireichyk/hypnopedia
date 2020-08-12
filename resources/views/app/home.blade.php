0a662b952b2ea05b9ae253c0a62bd8966bafe8e0
Andrei <dnsxss@gmail.com>
2020-08-12 13:27:06 +0300

    fix

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -59,7 +59,7 @@
                     smart alarm clock will wake you up at the most convenient time before the set wake-up time
                 </p>
                 <p class="sc-2__action">
-                    <a class="btn" href="https://go.onelink.me/app/18870943" target="_blank">
+                    <a class="btn" href="https://hypnopedia.onelink.me/gHSS/f769efa4" target="_blank">
                         Try for free
                     </a>
                 </p>
@@ -135,7 +135,7 @@
                     </div>
                 </div>
                 <div class="sc-3__action">
-                    <a class="btn" href="https://go.onelink.me/app/73f39e5a" target="_blank">Try for free</a>
+                    <a class="btn" href="https://hypnopedia.onelink.me/gHSS/f769efa4" target="_blank">Try for free</a>
                 </div>
             </div>
         </section>
