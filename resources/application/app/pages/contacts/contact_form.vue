75f8ff810445c3d3fa3caf5ef6b07c410df1aec9
Andrei <dnsxss@gmail.com>
2020-09-22 17:28:12 +0300

    fix

---
+++ resources/application/app/pages/contacts/contact_form.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="contacts__form">
+        <div class="form" v-if="!accept">
+            <h1 class="form__title">Contact</h1>
+            <div class="form__input" :class="{'has-error': error.name.length }">
+                <input placeholder="Name" v-model="name">
+                <small>{{ error.name }}</small>
+            </div>
+            <div class="form__input" :class="{'has-error': error.contact.length }">
+                <input placeholder="Contact" v-model="contact">
+                <small>{{ error.contact }}</small>
+            </div>
+            <div class="form__input" :class="{'has-error': error.message.length }">
+                <textarea placeholder="Message" v-model="message"/>
+                <small>{{ error.message }}</small>
+            </div>
+            <div class="form__checkbox">
+                <input id="accept" type="checkbox" v-model="accept_policy">
+                <label for="accept">I accept personal data processing agreement</label>
+            </div>
+            <div class="form__action"><a class="form__btn" href="javascript:void(0)" :class="{'disabled': !accept_policy || loading }" @click.stop="send">Submit</a></div>
+        </div>
+        <div class="accept" v-else><a class="accept__close" href="javascript:void(0)" @click.stop="accept=false"/><img class="accept__icon" src="./assets/accept.svg" alt="accept">
+            <p class="accept__text">Excellent! We'll contact you soon.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: "contact_form",
+        data() {
+            return {
+                accept: false,
+                accept_policy: false,
+                loading: false,
+                name: "",
+                contact: "",
+                message: "",
+                error: {
+                    name: "",
+                    contact: "",
+                    message: ""
+                }
+            }
+        },
+        methods: {
+            send() {
+
+                if (!this.accept_policy || this.loading) {
+                    return false;
+                }
+
+                this.loading = true;
+
+                axios.post('/api/contacts', {
+                    name: this.name,
+                    contact: this.contact,
+                    message: this.message
+                }).then(res => {
+                    this.loading = false;
+                    this.accept = true;
+                    this.clear();
+                }).catch(error => {
+                    let data = error.response.data;
+
+                    for (let key in data.errors) {
+                        this.error[key] = '';
+                        setTimeout(e => {
+                            this.error[key] = data.errors[key][0];
+                        }, 100);
+                    }
+
+                    this.accept = false;
+                    this.loading = false;
+                });
+
+
+            },
+            clear() {
+                this.name = null;
+                this.contact = null;
+                this.message = null;
+            }
+        },
+        watch: {
+            name: function () {
+                this.error.name = "";
+            },
+            contact: function () {
+                this.error.contact = "";
+            },
+            message: function () {
+                this.error.message = "";
+            }
+        },
+        mounted() {
+            $(function () {
+                $('textarea').each(function () {
+                    this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+                }).on('input', function () {
+                    this.style.height = 'auto';
+                    this.style.height = (this.scrollHeight) + 'px';
+                });
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/app/contacts.blade.php
+++ resources/views/app/contacts.blade.php
@@ -23,35 +23,14 @@
                 <a class="address__point" href="mailto:info@hypnopedia.site">info@hypnopedia.site</a>
                 <div class="address__socials">
                     <a class="address__social fb" href="https://www.facebook.com/hypnopedia.application" target="_blank">
-                        <img src="{{asset('/images/fbook.svg')}}">
+                        <img src="{{asset('/images/fbook.svg')}}" alt="facebook">
                     </a>
                     <a class="address__social tw" href="https://twitter.com/Hypnopedia_app" target="_blank">
-                        <img src="{{asset('/images/1200px-Twitter.png')}}">
+                        <img src="{{asset('/images/1200px-Twitter.png')}}" alt="Twitter">
                     </a>
                 </div>
             </div>
-            <div class="contacts__form" id="js-form">
-                <div class="form" v-if="!accept">
-                    <h1 class="form__title">Contact</h1>
-                    <div class="form__input" :class="{'has-error': error.name.length }">
-                        <input placeholder="Name" v-model="name"><small>@{{ error.name }}</small>
-                    </div>
-                    <div class="form__input" :class="{'has-error': error.contact.length }">
-                        <input placeholder="Contact" v-model="contact"><small>@{{ error.contact }}</small>
-                    </div>
-                    <div class="form__input" :class="{'has-error': error.message.length }">
-                        <textarea placeholder="Message" v-model="message"></textarea><small>@{{ error.message }}</small>
-                    </div>
-                    <div class="form__checkbox">
-                        <input id="accept" type="checkbox" v-model="accept_policy">
-                        <label for="accept">I accept personal data processing agreement</label>
-                    </div>
-                    <div class="form__action"><a class="form__btn" href="javascript:void(0)" :class="{'disabled': !accept_policy || loading }" @click.stop="send">Submit</a></div>
-                </div>
-                <div class="accept" v-else><a class="accept__close" href="javascript:void(0)" @click.stop="accept=false"></a><img class="accept__icon" src="/images/accept.svg">
-                    <p class="accept__text">Excellent! We'll contact you soon.</p>
-                </div>
-            </div>
+            <div id="contacts_form"></div>
         </div>
     </main>
 @endsection
