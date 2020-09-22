6afc2ad3f87603c2b870d0c05eeb7b52427e0516
Andrei <dnsxss@gmail.com>
2020-09-22 16:47:10 +0300

    fix

--- resources/views/app/layout/layout.blade.php
+++ resources/views/app/layout/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('/images/favicon.png')}}?{{env('VERSION')}}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/images/apple-touch-icon.png')}}?{{env('VERSION')}}/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/images/favicon-32x32.png')}}?{{env('VERSION')}}">
